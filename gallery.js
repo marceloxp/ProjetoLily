680345d079bc526b2e084bebe2b0f22474f7839a
Marcelo XP <marceloxp@gmail.com>
2023-04-11 14:32:46 -0300

    - Added all images.

--- gallery.js
+++ gallery.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var template = document.getElementById("item-template");
     var compiled = _.template(template.innerHTML);
     var images = [];
-    for (let i = 1; i <= 20; i++) {
+    for (let i = 1; i <= 1000; i++) {
         let image = `images/image-${i.toString().padStart(4, '0')}.jpg`;
         images.push({
             src: image
