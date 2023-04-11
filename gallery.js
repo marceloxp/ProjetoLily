5aad06df691490ba05b517c124fd942538f28fb0
Marcelo XP <marceloxp@gmail.com>
2023-04-11 14:31:21 -0300

    - Added underscorejs to template images.

---
+++ gallery.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var template = document.getElementById("item-template");
+    var compiled = _.template(template.innerHTML);
+    var images = [];
+    for (let i = 1; i <= 20; i++) {
+        let image = `images/image-${i.toString().padStart(4, '0')}.jpg`;
+        images.push({
+            src: image
+        });
+    };
+    const data = {
+        images: images
+    };
+    var html = compiled(data);
+    var container = document.querySelector('#grid-lily');
+    container.innerHTML = html;
+});
