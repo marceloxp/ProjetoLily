d770da6849f7671202532dfc4f19b758949eb24c
gustavo <gtslxp@gmail.com>
2023-04-14 16:30:43 -0300

    pre-final version

--- gallery.js
+++ gallery.js
@@ -1,19 +1,96 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var template = document.getElementById("item-template");
-    var compiled = _.template(template.innerHTML);
-    var images = [];
-    for (let i = 1; i <= 1000; i++) {
-        let image = `images/image-${i.toString().padStart(4, '0')}.jpg`;
-        images.push({
-            src: image
-        });
-    };
-    const data = {
-        images: images
+// document.addEventListener("DOMContentLoaded", function () {
+//   var template = document.getElementById("item-template");
+//   var compiled = _.template(template.innerHTML);
+//   var images = [];
+//   for (let i = 1; i <= 1000; i++) {
+//     let image = `images/image-${i.toString().padStart(4, "0")}.jpg`;
+//     images.push({
+//       src: image,
+//     });
+//   }
+//   const data = {
+//     images: images,
+//   };
+//   var html = compiled(data);
+//   var container = document.querySelector("#grid-lily");
+//   container.innerHTML = html;
+
+//   lightboxImages();
+// });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  let template = document.getElementById("item-template");
+  let compiled = _.template(template.innerHTML);
+  let images = [];
+  for (let i = 1; i <= 1000; i++) {
+    let image = `images/thumbs/image-${i.toString().padStart(4, "0")}.jpg`;
+    images.push({
+      src: image,
+    });
+  }
+  let container = document.querySelector("#grid-lily");
+  const totalPages = 10;
+  const imagesPerPage = 100;
+  let page = 1;
+  function showPage(page) {
+    let startIndex = (page - 1) * imagesPerPage;
+    let endIndex = startIndex + imagesPerPage;
+    let pageImages = images.slice(startIndex, endIndex);
+    var data = {
+      images: pageImages,
     };
     var html = compiled(data);
-    var container = document.querySelector('#grid-lily');
     container.innerHTML = html;
-
     lightboxImages();
+  }
+  showPage(1);
+  // Previous and next buttons
+  const prevBtn = document.createElement("button");
+  prevBtn.innerHTML = '<i class="fa-solid fa-chevron-left"></i>';
+  prevBtn.classList.add("lightbox-btn", "previous-btn", "btn-pag-prev");
+  prevBtn.disabled = true;
+  prevBtn.addEventListener("click", () => {
+    page--;
+    showPage(page);
+    prevBtn.disabled = page === 1;
+    nextBtn.disabled = false;
+    smoothlyScrollToTop();
+  });
+  const nextBtn = document.createElement("button");
+  nextBtn.innerHTML = '<i class="fa-solid fa-chevron-right"></i>';
+  nextBtn.classList.add("lightbox-btn", "next-btn", "btn-pag-next");
+  if (totalPages === 1) {
+    nextBtn.disabled = true;
+  }
+  nextBtn.addEventListener("click", () => {
+    page++;
+    showPage(page);
+    nextBtn.disabled = page === totalPages;
+    prevBtn.disabled = page === 1;
+    smoothlyScrollToTop();
+  });
+  const pagination = document.createElement("div");
+  pagination.classList.add("pagination");
+  pagination.appendChild(prevBtn);
+  pagination.appendChild(nextBtn);
+  document.querySelector(".gallery__wrapper").appendChild(pagination);
+
+  // Smooth scroll to top function
+  function smoothlyScrollToTop() {
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollStep = scrollHeight / 3;
+    let scrollInterval = setInterval(() => {
+      if (window.scrollY <= 0) {
+        clearInterval(scrollInterval);
+      }
+      window.requestAnimationFrame(() => {
+        window.scrollTo(0, window.scrollY - scrollStep);
+        if (window.scrollY + window.innerHeight < scrollHeight - 1) {
+          clearInterval(scrollInterval);
+          window.scrollTo(0, 0);
+        }
+      });
+    }, 5);
+  }
 });
--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-let images;
-const lightbox = document.getElementById("lightbox");
-const lightboxImg = lightbox.querySelector("img");
-const previousButton = document.querySelector(".previous-btn");
-const nextButton = document.querySelector(".next-btn");
-let currentImageIndex = 0;
-
-function lightboxImages() {
+ let images;
+ const lightbox = document.getElementById("lightbox");
+ const lightboxImg = lightbox.querySelector("img");
+ const previousButton = document.querySelector(".previous-btn");
+ const nextButton = document.querySelector(".next-btn");
+ let currentImageIndex = 0;
+ function lightboxImages() {
   images = document.querySelectorAll(".lightbox-image");
   images.forEach((image, index) => {
     image.addEventListener("click", (e) => {
@@ -14,62 +13,34 @@ function lightboxImages() {
       currentImageIndex = index;
     });
   });
+   const exitButton = document.getElementById("exit-image");
+  exitButton.addEventListener("click", () => {
+    lightbox.classList.remove("active");
+  });
 }
-
-previousButton.addEventListener("click", (e) => {
-  currentImageIndex--;
-  if (currentImageIndex < 0) {
-    currentImageIndex = images.length - 1;
-  }
-  lightboxImg.src = images[currentImageIndex].src;
-});
-
-nextButton.addEventListener("click", (e) => {
-  currentImageIndex++;
-  if (currentImageIndex >= images.length) {
-    currentImageIndex = 0;
-  }
-  lightboxImg.src = images[currentImageIndex].src;
-});
-
-lightbox.addEventListener("click", (e) => {
-  if (e.target !== e.currentTarget) return;
-  lightbox.classList.remove("active");
-});
-
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-// =-                 S C R O L L                 -=
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-const arrowIcon = document.querySelector(".arrow");
-const aboutSection = document.querySelector(".about");
-
-arrowIcon.addEventListener("click", () => {
-  aboutSection.scrollIntoView({ behavior: "smooth" });
-});
-
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-// =-              G O   T O   T O P              -=
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-// Get the button:
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+ previousButton.addEventListener("click", (e) => {
+   currentImageIndex--;
+   if (currentImageIndex < 0) {
+     currentImageIndex = images.length - 1;
+   }
+   lightboxImg.src = images[currentImageIndex].src;
+ });
+ nextButton.addEventListener("click", (e) => {
+   currentImageIndex++;
+   if (currentImageIndex >= images.length) {
+     currentImageIndex = 0;
+   }
+   lightboxImg.src = images[currentImageIndex].src;
+ });
+ lightbox.addEventListener("click", (e) => {
+   if (e.target !== e.currentTarget) return;
+   lightbox.classList.remove("active");
+ });
+ // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+ // =-                 S C R O L L                 -=
+ // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+ const arrowIcon = document.querySelector(".arrow");
+ const aboutSection = document.querySelector(".about");
+ arrowIcon.addEventListener("click", () => {
+   aboutSection.scrollIntoView({ behavior: "smooth" });
+ });
